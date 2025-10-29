abd4181725a4fde5eb4a734008760cc9dd0f02fa
guigncy <guigncy@gmail.com>
2025-10-29 15:00:10 -0300

    Adicionando os arquivos da estrutura do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.scss';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import TopAnimes from './pages/TopAnimes';
+import SearchAnime from './pages/SearchAnime';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/top-animes" element={<TopAnimes />} />
+        <Route path="/search" element={<SearchAnime />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
